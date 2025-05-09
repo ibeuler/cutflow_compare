0b8fef321402d3165396d82b265f18f1909838b9
ibeuler <ibohamed27@gmail.com>
2025-05-09 11:04:06 +0300

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cutflow_compare',
-    version='0.1.3',
+    version='0.1.4',
     author='Ibrahim H.I. ABUSHAWISH',
     author_email='ibrahim.hamed2701@gmail.com',
     description='A package to compare cutflow histograms from ROOT files.',
